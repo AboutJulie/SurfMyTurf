b6cef6d8cb414f5f7207e9eeebaa208b2a61e3d6
Tomas Trzicky <tomas.trzicky1@seznam.cz>
2020-11-10 15:41:09 +0000

    test

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -6,12 +6,15 @@ class SurfboardsController < ApplicationController
   end
 
   def create
+    raise
   end
 
   def update
   end
 
   def new
+    @surfboard = Surfboard.new
+    @user = current_user
   end
 
   def edit
