f022cec14d5611a065d2487bdfc655b6a04721a0
Nils Bruder <nils.bruder@outlook.com>
2020-11-10 16:52:22 +0000

    seeds initialized with fake data

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -2,5 +2,5 @@ class Surfboard < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates: :name, :price, :location, :availability, :description presence: true
+  validates :name, :price, :location, :availability, :description, presence: true
 end
