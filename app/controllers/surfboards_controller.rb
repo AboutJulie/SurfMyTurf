13764efda9614349a3718ceab5bf9460fde271f2
Tomas Trzicky <tomas.trzicky1@seznam.cz>
2020-11-10 22:15:25 +0000

    bug of creating surfboard solved

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -3,13 +3,7 @@ class SurfboardsController < ApplicationController
   end
 
   def show
-  end
-
-  def create
-    raise
-  end
-
-  def update
+    @surfboard = Surfboard.find(params[:id])
   end
 
   def new
@@ -17,6 +11,31 @@ class SurfboardsController < ApplicationController
     @user = current_user
   end
 
+  def create
+    @surfboard = Surfboard.new(surfboard_params)
+    @surfboard.user = current_user
+
+    if @surfboard.save
+      redirect_to surfboards_path, notice: "Your board has been added!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @surfboard = Surfboard.find(params[:id])
+    @surfboard.update(surfboard_params)
+
+    redirect_to surfboards_path(@restaurant)
+  end
+
   def edit
+    @surfboard = Surfboard.find(params[:id])
+  end
+
+  private
+
+  def surfboard_params
+    params.require(:surfboard).permit(:name, :description, :location, :price) #, :availability
   end
 end
