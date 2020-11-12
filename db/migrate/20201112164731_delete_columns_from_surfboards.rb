75a735ab2abee1cfb93a46be34b8399986577f1f
Nils Bruder <nils.bruder@outlook.com>
2020-11-12 17:08:33 +0000

    deletemigrationfile

--- db/migrate/20201112164731_delete_columns_from_surfboards.rb
+++
@@ -1,6 +0,0 @@
-class DeleteColumnsFromSurfboards < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :surfboards, :latitude, :float
-    remove_column :surfboards, :longitude, :float
-  end
-end
