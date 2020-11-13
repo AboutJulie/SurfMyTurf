a1f2927c0d312d73fa12758a52614afdc51bef73
Julia Kauffmann <kauffmann.julia@yahoo.de>
2020-11-13 11:00:52 +0000

    frontend-booking-form

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,3 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
 
-#THis is test 2
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :surfboard
+
+  validates :start_date, :end_date, presence: true
 end
